f25d854e1df79e3c28eabe1f64224a66017f2d6e
Alvin Cheng <ascheng21@gmail.com>
2017-06-29 16:27:43 -0700

    Car shows on canvas on page load. #1

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 const $canvas = document.querySelector('#canvas')
-var context = $canvas.getContext('2d')
+const context = $canvas.getContext('2d')
 context.fillStyle = '#ecf0f1'
 context.fillRect(10, 10, $canvas.width, $canvas.height)
+
+class Car {
+  constructor(direction, speed, location) {
+    this.direction = direction
+    this.speed = speed
+    this.location = location
+  }
+
+  render() {
+    context.fillStyle = 'black'
+    context.fillRect($canvas.width / 2, $canvas.height / 2, 10, 20)
+  }
+}
+
+window.addEventListener('load', function (event) {
+  const user = new Car('up', 0, [0, 0])
+  user.render()
+})
